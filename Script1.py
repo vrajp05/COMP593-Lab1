5dcdc51de8e9b3656d12a7334075519d8361b110
vrajp05 <156490805+vrajp05@users.noreply.github.com>
2024-01-17 11:26:28 -0500

    Updated scriptssss

--- Script1.py
+++ Script1.py
@@ -1,3 +1,2 @@
 print("This is the first script for GitHub")
-print("12345")
 #just a random comment
--- Script2.py
+++ Script2.py
@@ -1,2 +1,4 @@
 print("This is the mandatory command for the first lab pof scripting")
 #2nd script of ther lab
+print("objects")
+print("web developer")
